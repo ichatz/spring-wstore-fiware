776763aa6d5d5e748f7368216b5fbc92cdf2c845
Ioannis Chatzigiannakis <ichatz@github.com>
2015-10-21 17:16:02 +0100

    integrating wstore

--- src/main/java/com/ichatz/wstore/fiware/api/UserController.java
+++ src/main/java/com/ichatz/wstore/fiware/api/UserController.java
@@ -1,11 +1,14 @@
 package com.ichatz.wstore.fiware.api;
 
-import com.ichatz.wstore.fiware.model.FiwareProfile;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ichatz.wstore.fiware.model.FiwareProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.stereotype.Controller;
@@ -28,9 +31,25 @@ public class UserController {
         return profile;
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/store", method = RequestMethod.GET)
+    public String getOfferings() throws JsonProcessingException {
+        final String url = "http://fff.sparkworks.net/api/administration/marketplaces";
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + oauth2RestTemplate.getAccessToken());
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        final HttpEntity<String> response = oauth2RestTemplate.exchange(
+                url, HttpMethod.GET, entity, String.class);
+
+        return response.getBody();
+    }
+
+
     private FiwareProfile getProfile() {
-        LOGGER.debug("get profile");
-        String url = "https://account.lab.fiware.org/user?access_token=" + oauth2RestTemplate.getAccessToken();
+        final String url = "https://account.lab.fiware.org/user?access_token=" + oauth2RestTemplate.getAccessToken();
         ResponseEntity<FiwareProfile> forEntity = oauth2RestTemplate.getForEntity(url, FiwareProfile.class);
         return forEntity.getBody();
     }
